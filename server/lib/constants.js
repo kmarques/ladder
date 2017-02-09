0514414f408e05273972f730b300e7e95c1346e8
Karl Marques <kmarques@hi-media.com>
2017-02-09 16:05:06 +0100

    fix node export

--- server/lib/constants.js
+++ server/lib/constants.js
@@ -2,5 +2,7 @@
  * Created by kmarques on 09/02/2017.
  */
 
-export const SECRET = process.env.JWT_TOKEN || 'jwt_token';
-export const MONGO = process.env.MONGOLAB_URI || 'mongodb://localhost/ladder';
+exports = {
+  SECRET: process.env.JWT_TOKEN || 'jwt_token',
+  MONGO: process.env.MONGOLAB_URI || 'mongodb://localhost/ladder'
+};
